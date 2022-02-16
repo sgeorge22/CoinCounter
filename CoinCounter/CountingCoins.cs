6509d9fc2330944c521915cc82cd9670ccb78e1e
sarah george <sgeorge@tql.com>
2022-02-15 20:46:31 -0500

    worked on method for coin counter

--- CoinCounter/CountingCoins.cs
+++ CoinCounter/CountingCoins.cs
@@ -8,10 +8,22 @@ namespace CoinCounter
 {
     public class CountingCoins
     {
-
         public void GenerateCoinCounter()
         {
+            //ask the user to input a number
+            Console.WriteLine("Welcome to Coin Counter! \n");
+            Console.WriteLine("Please enter a number to be used as a cent value: ");
+            string userValue = Console.ReadLine();
 
+            //convert user string to double
+            double userValueNumber = Convert.ToDouble(userValue);
+
+            //assign values to coin type variables
+            double quaterValue = 25;
+            double dimeValue = 10;
+            double nickleValue = 5;
+
+            double quaterCoin = Math.Floor(userValueNumber / quaterValue);
         }
     }
 }
--- CoinCounter/Program.cs
+++ CoinCounter/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CoinCounter;
 
+var coinCounter = new CountingCoins();
+
+coinCounter.GenerateCoinCounter();
